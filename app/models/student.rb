064299ff060790aa0c081e78525e0879a0e38758
Jacob Lepler <lepler.jacob@gmail.com>
2021-02-05 21:33:39 -0500

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,11 @@
 class Student < ActiveRecord::Base
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def is_active?
+    self.active ? "active" : "inactive"
+  end
+
 end
---
+++ db/migrate/002_add_column_active_to_students_table.rb
@@ -0,0 +1,5 @@
+class AddColumnActiveToStudentsTable < ActiveRecord::Migration
+    def change
+        add_column :students, :active, :boolean, default: false 
+    end
+end
